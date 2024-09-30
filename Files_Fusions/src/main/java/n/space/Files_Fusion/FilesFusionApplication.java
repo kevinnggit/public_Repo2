d89a0c4f49e50ee193e6045051cee71521e4c3dd
kevnguefackdjoukeng <kevnguefackdjoukeng@smail.hs-bremerhaven.de>
2024-09-30 14:30:47 +0200

    habe geprüft, die einzulesende Dateien existieren

--- Files_Fusions/src/main/java/n/space/Files_Fusion/FilesFusionApplication.java
+++ Files_Fusions/src/main/java/n/space/Files_Fusion/FilesFusionApplication.java
@@ -3,11 +3,15 @@ package n.space.Files_Fusion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import n.space.Files_Fusion.space.nevik.files.Read_files;
+
 @SpringBootApplication
 public class FilesFusionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilesFusionApplication.class, args);
+		Read_files lese = new Read_files();
+		System.out.println(lese);
 	}
 
 }
--- Files_Fusions/src/main/java/n/space/Files_Fusion/space/nevik/files/read_files.java
+++ Files_Fusions/src/main/java/n/space/Files_Fusion/space/nevik/files/read_files.java
@@ -1,6 +1,18 @@
 package n.space.Files_Fusion.space.nevik.files;
 import java.util.Scanner;
 
-public class read_files {
-    scanner sc = new scanner(System.in);
+import javax.sound.sampled.SourceDataLine;
+
+import java.io.File;
+
+public class Read_files {
+
+    File docu;
+    public boolean a;
+
+    public Read_files(){
+    docu = new File("C:/Users/kevin/Desktop/Team17Endbericht.pdf");
+    a = docu.isFile();
+    System.out.println(docu.isFile());
+    }
 }
